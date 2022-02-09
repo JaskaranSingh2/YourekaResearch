1fb5278b18f18fc9125bcb149e2ad5c6e8a3948b
JforJason101 <jasonkaura@gmail.com>
2022-02-08 20:04:18 -0700

    com

---
+++ Session2.R
@@ -0,0 +1,4 @@
+install.packages('tidyverse', dependencies = TRUE)
+install.packages('gapminder', dependencies = TRUE)
+install.packages('ggsignif', dependencies = TRUE)
+install.packages('ggpubr', dependencies = TRUE)
