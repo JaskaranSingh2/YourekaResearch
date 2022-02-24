f60cd6ba1341d3454573bb67b82e8aa766cebe2d
JforJason101 <jasonkaura@gmail.com>
2022-02-23 22:59:14 -0700

    com

--- Comparison/test.py
+++ Comparison/test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 arr = pd.read_excel('compare.xlsx')
@@ -11,6 +12,12 @@ print(x)
 x = list(range(1, x+1))
 print(x)
 
-plt.scatter(x, y1, s=3, c="red")
-plt.scatter(x, y2, s=3, c="blue")
+best = np.poly1d(np.polyfit(x, y2, 3))
+curve = np.linspace(1, 3193, 100)
+plt.plot(curve, best(curve), color="orange", linewidth="3")
+
+plt.scatter(x, y1, s=3, c="red", alpha=0.6)
+plt.scatter(x, y2, s=3, c="blue", alpha=0.6)
+plt.xlabel(
+    "Each individual 3193 counties (not named because of the lack of space")
 plt.show()
